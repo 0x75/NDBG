74fedc1f242447878e469ebf178027ed39c7c2f3
jnengel <jnengel@web.de>
2013-07-02 12:28:36 +0200

    Initial Commit

---
+++ NDBG/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  NDBG
+//
+//  Created by Nico on 02.07.13.
+//  Copyright (c) 2013 JNE. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
